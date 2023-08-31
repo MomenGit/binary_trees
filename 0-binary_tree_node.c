1670af08e2d831ab1ee8249f7636ddb0320e43e5
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-08-31 12:22:22 +0300

    Write 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - ...
+ *
+ * @parent: ...
+ * @value: ...
+ * Return: binary_tree_t* on success, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return;
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
