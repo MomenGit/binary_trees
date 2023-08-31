e2bdd6d49882b82b05964cf61c83f399551dc553
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-08-31 23:22:10 +0300

    Write 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,4 +9,17 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
+
+	if (tree == NULL)
+		return (0);
+
+	depth = 0;
+	while (tree->parent != NULL)
+	{
+		depth += 1;
+		tree = tree->parent;
+	}
+
+	return (depth);
 }
