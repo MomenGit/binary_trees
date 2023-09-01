5d71fbeeea44b925fb266ed82aca1dc21000c531
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-01 10:43:43 +0300

    Write 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 
 /**
+ * calc_height - measures the height of a binary tree
+ *
+ * @tree: a pointer to root node of the tree to measure the height
+ * Return: size_t height of a binary tree, or 0 on failure
+ */
+long calc_height(const binary_tree_t *tree)
+{
+	long left_h, right_h;
+
+	if (tree == NULL)
+		return (-1);
+
+	left_h = calc_height(tree->left);
+	right_h = calc_height(tree->right);
+
+	if (left_h > right_h)
+		return (left_h + 1);
+	else
+		return (right_h + 1);
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  *
  * @tree: a pointer to root node of the tree to measure the height
@@ -9,4 +31,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
+	return (calc_height(tree));
 }
