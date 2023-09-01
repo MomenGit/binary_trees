67a9ae51333ea6cb39879ff7aafc2abbe5a907e2
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-01 14:49:38 +0300

    Write 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,4 +9,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t left_leaves, right_leaves;
+
+	if (tree == NULL)
+		return (0);
+
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (left_leaves + right_leaves + 1);
+	else
+		return (left_leaves + right_leaves);
 }
