f9347ed684d85a56b521103fe1467ae0ba909256
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-01 14:43:36 +0300

    Write 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,4 +9,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t left_size, right_size;
+
+	if (tree == NULL)
+		return (0);
+
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return (right_size + left_size + 1);
 }
