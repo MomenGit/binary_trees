1d1a6516d0c934db357e0918e3ebc4f2d743cd6f
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-01 17:46:23 +0300

    Write 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,4 +9,21 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left_t, right_t;
+
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left != NULL && tree->right == NULL) ||
+		(tree->left == NULL && tree->right != NULL))
+		return (0);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		left_t = binary_tree_is_full(tree->left);
+		right_t = binary_tree_is_full(tree->right);
+		return (left_t && right_t);
+	}
+
+	return (1);
 }
