7eb50cb5275504162286c883c6d73f5f12a465c5
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-01 17:46:23 +0300

    Write 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,4 +9,21 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grand_parent;
+
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent == NULL)
+		return (NULL);
+
+	grand_parent = node->parent->parent;
+
+	if (grand_parent == NULL)
+		return (NULL);
+
+	if (node->parent == grand_parent->left)
+		return (grand_parent->right);
+	else
+		return (grand_parent->left);
 }
